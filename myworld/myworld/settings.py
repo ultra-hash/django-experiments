40bfd6bf68bbb3e65627f9336137e30e99b383ff
veerabhadra swamy uppu <78792323+ultra-hash@users.noreply.github.com>
2022-07-29 23:37:58 +0530

    Update settings.py

--- myworld/myworld/settings.py
+++ myworld/myworld/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=nyoq0i3in=j-2m@6_hg!18^if9o^qup$)f%df$xp7%6mr4)wj'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
